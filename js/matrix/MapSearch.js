6c4d20e8baa75dffc7f6dc9b2174fd0aa23a42f3
Jason Lieu <jlieu@esri.com>
2014-05-15 14:30:50 -0400

    made portal connection and results show

--- js/matrix/MapSearch.js
+++ js/matrix/MapSearch.js
@@ -4,19 +4,37 @@ define([
   "dijit/_WidgetBase",
   "dijit/_TemplatedMixin",
   "dijit/_WidgetsInTemplateMixin",
-  "dojo/text!./templates/MapSearch.html"
+  "dojo/text!./templates/MapSearch.html",
+  //
+  "dojo/on",
+  "esri/config",
+  "esri/arcgis/Portal",
+  "dojo/dom",
+  "dojo/_base/json",
+  "dojo/_base/lang",
+  "dojo/_base/array",
+  'dojo/dom-construct'
 ], function(
   declare,
   domAttr,
   _WidgetBase,
   _TemplatedMixin,
   _WidgetsInTemplateMixin,
-  template
+  template,
+  //
+  on,
+  config,
+  esriPortal,
+  dom,
+  JSON,
+  lang,
+  array,
+  domConstruct
 ) {
 
-  var ResultItem = declare([], {
+    var ResultItem = declare([_WidgetBase, _TemplatedMixin], {
 
-    templateString: "<li>${label}</li>",
+        templateString: "<li data-itemid='${itemId}'>${label}</li>",
 
     label: "foo",
 
@@ -28,36 +46,94 @@ define([
 
     templateString: template,
     baseClass: "map-search",
+    portal: null,
+    portalUrl: 'http://www.arcgis.com',
 
     startup: function() {
-      this.inherited(arguments);
+        this.inherited(arguments);
+        this._connectToPortal();
 
       
+    },
+    
+    _searchPortal: function () {
+        console.log('is searching');
+        if (this.portal) {
+            console.log('searching');
+            var params = {
+                q: domAttr.get(this.searchTextNode, "value"),
+                num: 5,
+                type: 'Web Map'
+            };
+            this.portal.queryItems(params).then(lang.hitch(this, function(result){
+               // console.log('success', (result));
+                this._onResults(result.results);
+            }));
+        }
+    },
+
+    _connectToPortal: function () {
+        if (this.portalUrl) {
+            //create the portal
+           var portal = new esriPortal.Portal(this.portalUrl);
+
+           on(portal, 'load', lang.hitch(this, function (p) {
+               console.log('portal loaded');
+               this.portal = p;
+               console.log(this);               
+            }));
+        } else {
+            console.error("invalid portal url");
+        }
+       
     },
 
     _onClick: function() {
 
-      // var value = domAttr.get(this.searchTextNode, "value");
+        var value = domAttr.get(this.searchTextNode, "value");
+        if (this.portal) {
+            //search portal
+            this._searchPortal();
+        } else {
+            //connect to portal then search portal
+            this._connectToPortal();
+            this._searchPortal();
+
+        }
 
 
-      // esri/arcgis/Portal::queryItems( params ).then(lang.hitch(this, _onResults));
+      //esri/arcgis/Portal::queryItems( params ).then(lang.hitch(this, _onResults));
 
     },
 
     _onResults: function(results) {
+        console.log('results', results);
 
-      // dojo/_base/array::forEach(results, function(result) {
+        array.forEach(results, lang.hitch(this, function (result, i) {
+            console.log(result);
+            var item = new ResultItem({
+                label: result.title,
+                itemId: result.id
+            });
+            console.log(item);
+            domConstruct.place(item.domNode, this.resultsListNode, 'last');
+        }));
+        //cycle through array
+        //create <li> for each result
+        //add <li> to resultsListNode
+
+        // dojo/_base/array::forEach(results, function(result) {
 
         // create ResultItem in this.resultsListNode
         // don't forget to call ::startup()
 
-      //})
+        //})
 
-    }
+    },
 
     _onItemClick: function() {
 
-      this.onItemSelect();
+        this.onItemSelect();
 
     },
 
